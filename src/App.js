30a063fbc0da96b91cd7cb0932236a5891069753
Faizal Amiruddin <faizal@wowrack.com>
2022-02-27 20:44:28 +0700

    feat(app): clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Index from './pages/products/Index';
 import Details from './pages/products/Detail';
@@ -6,23 +6,21 @@ import Form from './pages/products/Form';
 
 function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/product"/>}/>
-        <Route path="product" element={<Layout title="Product"/>}>
-          <Route index element={<Index/>}/>
-          <Route path=":id" element={<Details/>}/>
-          <Route path="add" element={<Form/>}/>
-          <Route path="edit/:id" element={<Form/>}/>
-        </Route>
-        <Route
-          path="*"
-          element={
-            <Layout>Not Found.</Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/product"/>}/>
+      <Route path="product" element={<Layout title="Product"/>}>
+        <Route index element={<Index/>}/>
+        <Route path=":id" element={<Details/>}/>
+        <Route path="add" element={<Form/>}/>
+        <Route path="edit/:id" element={<Form/>}/>
+      </Route>
+      <Route
+        path="*"
+        element={
+          <Layout>Not Found.</Layout>
+        }
+      />
+    </Routes>
   );
 }
 
