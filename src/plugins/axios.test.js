55094d8d92b96b4ce8f22ff966d1eaa52e228fc7
Faizal Amiruddin <faizal@wowrack.com>
2022-03-03 16:27:19 +0700

    feat(http): setup axios

---
+++ src/plugins/axios.test.js
@@ -0,0 +1,27 @@
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import axiosRequest from './axios';
+
+test('axios request works with correct configs', async () => {
+  const sendPayload = { sent: 'hello' };
+  const respPayload = { response: 'hi' };
+
+  let reqHeader = null;
+  let reqBody = null;
+  const server = setupServer(
+    rest.post(`${process.env.REACT_APP_API_BASE_URL}/test`, (req, res, ctx) => {
+      reqHeader = req.headers.get('Content-Type');
+      reqBody = req.body;
+      return res(ctx.json({ ...respPayload }));
+    }),
+  );
+  server.listen();
+
+  const { data } = await axiosRequest.post('/test', { ...sendPayload });
+
+  expect(reqHeader).toBe('application/json');
+  expect(reqBody).toEqual(sendPayload);
+  expect(data).toEqual(respPayload);
+
+  server.close();
+});
