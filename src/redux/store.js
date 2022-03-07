f7f9643da6d7e9da1ec3640ae75ffa4134a1af9b
Faizal Amiruddin <faizal@wowrack.com>
2022-03-07 18:46:19 +0700

    feat(store): make configureStore more customizable

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,18 @@ import thunk from 'redux-thunk';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import productsReducer from './products/products.reducer';
 
+
+const middlewares = [thunk];
+
 const rootReducer = combineReducers({
   products: productsReducer,
 });
 
-export default function configureStore (preloadedState) {
-  const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+export default function configureStore (preloadedState, reducers = rootReducer) {
+  const store = createStore(reducers, preloadedState, applyMiddleware(...middlewares));
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./store', () => store.replaceReducer(rootReducer));
+    module.hot.accept('./store', () => store.replaceReducer(reducers));
   }
 
   return store;
